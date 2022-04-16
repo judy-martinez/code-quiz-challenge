dc14e75a480d2027c5e4d833b55324e9970f2337
Judy <judy.mtz1@gmail.com>
2022-04-16 02:00:14 -0500

    added questions

---
+++ questions.js
@@ -0,0 +1,28 @@
+// Quiz Questions
+var questions = [
+    {
+        question: `Commonly used data types DO Not include:`,
+        options: [`strings`, `booleans`, `alerts`, `numbers`],
+        answer: `alerts`
+    },
+    {
+        question: `The condition in an if/else statement is enclosed with ___.`,
+        options: [`quotes`, `curly crackets`, `parenthesis`, `square brackets`],
+        answer: `parenthesis`
+    },
+    {
+        question: `Arrays in JavaScript can be used to store:`,
+        options: [`numbers & strings`, `other arrays`, `booleans`, `all of the above`],
+        answer: `all of the above`
+    },
+    {
+        question: `string values must be enclosed within ____ when being assigned to variables`,
+        options: [`commas`, `curly brackets`, `quotes`, `parenthesis`],
+        answer: `quotes`
+    },
+    {
+        question: `A very useful took used during development and debugging for printing content to the debugger is:`,
+        options: [`JavaScript`, `terminal/bash`, `for loops`, `console.log`],
+        answer: `console.log`
+    }
+];
